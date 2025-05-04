ca755a8ebf179b867900690bbcd36adf75df08b5
levin <ivan.levin@ocrv.ru>
2025-05-04 18:04:13 +0300

    Реализация подключения к бд. Реализация базовой работой бд.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,21 @@
 package jm.task.core.jdbc;
 
+import java.io.IOException;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        try (UserServiceImpl userService = new UserServiceImpl()) {
+            userService.createUsersTable();
+            userService.saveUser("Fedor", "Fox", (byte) 22);
+            userService.saveUser("Ivan", "Bad", (byte) 44);
+            userService.saveUser("Alexandr", "Two", (byte) 32);
+            userService.saveUser("Vasa", "Good", (byte) 35);
+            System.out.println(userService.getAllUsers());
+            userService.cleanUsersTable();
+            userService.dropUsersTable();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/preproject";
+    private static final String USER = "erd";
+    private static final String PASSWORD = "erd";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("Ошибка при подключении к базе данных или выполнении запроса");
+            e.printStackTrace();
+        }
+        return null;
+    }
     // реализуйте настройку соеденения с БД
 }
